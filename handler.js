8752d62ac499dd68c3d400c511c369eea9dc08e7
harmy <hack4cn@gmail.com>
2018-11-06 17:29:44 +0800

    first commit

---
+++ handler.js
@@ -0,0 +1,30 @@
+// const puppeteerLib = require("./lib/puppeteerFunc");
+import puppeteerLib from "./lib/puppeteerFunc";
+
+export const showIP = async (event, context, callback) => {
+  // module.exports.showIP = async (event, context, callback) => {
+  let master = null;
+  console.log(event.Actor.ID);
+
+  try {
+    master = await puppeteerLib.getPuppeteerMaster(event);
+    console.log("getPuppeteerMaster init done");
+    const page = await master.newPage("https://api.ipify.org");
+
+    console.log("page goto done");
+    const html = await page.content();
+    console.log("page content done");
+    console.log(html);
+    event.result = "OK";
+    callback(null, event);
+  } catch (e) {
+    console.log(e);
+    callback(e);
+  } finally {
+    if (master != undefined) {
+      //关闭浏览器
+      console.log("关闭浏览器");
+      await master.close();
+    }
+  }
+};
